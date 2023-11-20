1da2aa3d9a44b2838de516fa5cd32dcc2d768ef6
Sonu sGate <sonu@sgate.net>
2023-11-20 13:51:52 +0530

    added pagination

--- Routes/BasicRoutes.js
+++ Routes/BasicRoutes.js
@@ -2,19 +2,27 @@ const express=require("express")
 const { pool } = require("../Models/BasicModel")
 const UserRouter=express.Router()
 
-UserRouter.get("/data",(req,res)=>{
+UserRouter.get("/data",async(req,res)=>{
  const {limit,page}=req.query
 const OFSET=(+page-1)*(+limit)
-
-    
+let alldata=0
+    pool.query("SELECT Count(name) FROM teacher",((error,results)=>{
+        if(error){
+            console.log(error)
+        }else{
+            // console.log(results)
+           alldata=results[0]["Count(name)"]
+        }
+    }))
     try{
-        
  pool.query (`SELECT * FROM teacher ${limit&&page&&"LIMIT ? OFFSET ?"}`, page&&limit&&[+limit,+OFSET],(error,results)=>{
     if(error){
         console.log(error)
+        
         res.status(400).json({msg:error})
     }else{
-        res.status(200).json(results)
+console.log(alldata)
+        res.status(200).json({results,[limit&&page&&"totalPages"]:Math.ceil((+alldata)/+limit)})
     }
 })
     }catch(err){
@@ -62,5 +70,20 @@ UserRouter.patch("/altertable",(req,res)=>{
     }))
 })
 // UserRouter.post('')
+UserRouter.patch("/edit/:id",(req,res)=>{
+    const {id}=req.params
+    const {name}=req.body
+    try{
+        pool.query('UPDATE teacher SET name=? WHERE id=?',[name,id],((error,results)=>{
+            if(error){
+                res.status(400).json({msg:error})
+            }else{
+                res.status(200).json({msg:results})
+            }
+        }))
+    }catch(err){
+        res.status(400).json({msg:err})
+    }
+})
 
 module.exports={UserRouter}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors=require("cors")
 const { pool } = require("./Models/BasicModel")
 const { UserRouter } = require("./Routes/BasicRoutes")
 
+
 const app=express()
 app.use(cors())
 app.use(express.json())
@@ -15,55 +16,11 @@ next()
 })
 
 
-// app.get("/teacher",(req,res)=>{
-//     pool.query("SELECT * FROM teacher",(error,results)=>{
-//         if(error){
-//             console.log(error)
-//             res.status(400).json({"msg":"something going wrong"})
-//         }
-//         else{
-//             res.status(200).json({msg:results})
-//         }
-//     })
-// })
-// app.post("/teacher",(req,res)=>{
-//     const {id,name,email,dept_id}=req.body
-//     pool.query("INSERT INTO teacher (id,name,email,dept_id) VALUES (?,?,?,?)",[
-//         id,name,email,dept_id
-//     ],(error,results)=>{
-//         if(error){
-//             res.status(400).json({msg:error})
-//         }else{
-//             res.status(200).json({msg:results})
-//         }
-//     })
-// })
-// app.delete("/teacher/:id",(req,res)=>{
-//     const {id}=req.params
-//     pool.query("DELETE  FROM teacher WHERE id=?",[id],(error,results)=>{
-//         if(error){
-//             res.status(400).json({msg:error})
-//         }else{
-//             res.status(200).json({msg:"DEleted successfully"})
-//         }
-//     })
-// })
-// app.patch("/techers/:id",async(req,res)=>{
-//     const {id}=req.params
-//     const {name}=req.body;
-//     console.log(id)
-//     req.mysql.query('UPDATE teacher SET name=? WHERE id=?',[name,id],(error,results)=>{
-//         if(error){
-//             res.status(400).json({msg:error})
-//         }else{
-//             res.status(200).json({msg:results})
-//         }
-//     })
-// })
+
 app.use("/teacher",UserRouter)
 app.listen(3000,async()=>{
     try{
-        await pool.connect((error)=>{
+       await pool.connect((error)=>{
             if(error){
                 console.log(error)
             }else{
